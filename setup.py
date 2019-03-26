1df957cd07e5a87ec23f66d099b4139e5fcbbb12
Damir Cavar <dcavar@me.com>
2019-03-26 17:50:23 -0400

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,26 @@
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="polyglotjsonnlp",
+    version="0.2.1",
+    author="Damir Cavar, Oren Baldinger, Maanvitha Gongalla, Anurag Kumar, Murali Kammili",
+    author_email="damir@cavar.me",
+    description="The Python Polyglot JSON-NLP package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dcavar/Polyglot-JSON-NLP",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'polyglot>=16.7.4',
+        'pyjsonnlp>=0.2.1'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+)

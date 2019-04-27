ec9faf828e6c4c4d6116da4caae759d0c1cdb813
Oren Baldinger <obalding@iu.edu>
2019-04-27 11:58:47 -0400

    model download script

---
+++ download_models.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+echo "downloading polyglot models"
+echo "https://polyglot.readthedocs.io/en/stable/Download.html#langauge-task-support for all supported"
+polyglot download LANG:en
+polyglot download LANG:de
+polyglot download LANG:es
+polyglot download LANG:zh
+polyglot download LANG:ar
+polyglot download LANG:hi
